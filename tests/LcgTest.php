bee2aa166945c342c803002b0f6c905a3e54bd6d
Arkadij Kuzhel <arkadij@madepeople.se>
2019-03-06 15:31:06 +0200

    Update PHPUnit
      * Define method input and return types
      * Add/update PhpDoc blocks
      * Reformat lines

--- tests/LcgTest.php
+++ tests/LcgTest.php
@@ -12,20 +12,21 @@ use Knowgod\PRNG\LinearCongruentialGenerator;
  */
 class LcgTest extends \PHPUnit\Framework\TestCase
 {
-    public function getGeneratorTestData()
+    /**
+     * @return array[]
+     */
+    public function getGeneratorTestData(): array
     {
         return [
             [
                 LinearCongruentialGenerator::bsd_rand(0),
                 LinearCongruentialGenerator::bsd_rand(0),
                 16,
-            ],
-            [
+            ], [
                 LinearCongruentialGenerator::bsd_rand(500800),
                 LinearCongruentialGenerator::bsd_rand(500800),
                 31, 4,
-            ],
-            [
+            ], [
                 LinearCongruentialGenerator::msvcrt_rand(500800),
                 LinearCongruentialGenerator::msvcrt_rand(500800),
                 31, 4,
@@ -36,11 +37,17 @@ class LcgTest extends \PHPUnit\Framework\TestCase
     /**
      * @dataProvider getGeneratorTestData
      *
-     * @param int $seed
-     * @param int $rowLength
+     * @param \Closure $randFunction1
+     * @param \Closure $randFunction2
+     * @param int      $rowLength
+     * @param int      $skipRowItems
      */
-    public function testRepeatability($randFunction1, $randFunction2, $rowLength, $skipRowItems = 0)
-    {
+    public function testRepeatability(
+        \Closure $randFunction1,
+        \Closure $randFunction2,
+        int $rowLength,
+        int $skipRowItems = 0
+    ) {
         for ($i = 0; $i < $rowLength; ++$i) {
             if ($i < $skipRowItems) {
                 continue;
